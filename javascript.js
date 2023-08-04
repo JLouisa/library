f6d7ea2eb1c52a66b26794083b8de4c5fd412e68
Jlouisa <jonathan.louisa@gmail.com>
2023-08-04 17:46:25 +0200

    Add function to add title to screen

--- javascript.js
+++ javascript.js
@@ -13,29 +13,54 @@ const printBooks = document.querySelector(".books");
 let i = 0;
 
 // ---------------------------
+const titleEl = document.querySelector("#addTitle");
+titleEl.addEventListener("input", updateValue);
+
+let titleInfo = "";
+function updateValue(e) {
+  titleInfo = e.target.value;
+}
+// ---------------------------
+
 function authorName(evt) {
   console.log(evt.key);
   const key = evt.key;
 }
 
 const authorEl = document.querySelector("#addAuthor");
-authorEl.addEventListener("keydown", authorName);
-// ---------------------------
-function titleName(evt) {
-  console.log(evt.key);
-  const key = evt.key;
+authorEl.addEventListener("input", updateValue);
+
+//----------------------------
+
+function myFunction() {
+  document.getElementById("leForm").reset();
 }
 
-const titleEl = document.querySelector("#addTitle");
-titleEl.addEventListener("keydown", titleName);
-// ---------------------------
-
-const addBtn = document.querySelector("addBtn");
-
-testLibrary.forEach((n) => {
-  console.log(n);
+function addScreen(n) {
   let titles = document.createElement("div");
   titles.classList.add("title", `nr${i++}`);
   titles.textContent = n;
   printBooks.appendChild(titles);
+}
+
+const addBtn = document.querySelector(".addBtn");
+addBtn.addEventListener("click", () => {
+  testLibrary.push(titleInfo);
+  console.log(testLibrary);
+  theForm.setAttribute("style", "visibility: hidden;");
+  addScreen(titleInfo);
+});
+// ---------------------------
+
+testLibrary.forEach((n) => {
+  let titles = document.createElement("div");
+  titles.classList.add("title", `nr${i++}`);
+  titles.textContent = n;
+  printBooks.appendChild(titles);
+});
+
+const newBtn = document.querySelector("#newBtn");
+const theForm = document.querySelector(".theForm");
+newBtn.addEventListener("click", () => {
+  theForm.setAttribute("style", "visibility: visible;");
 });
