e2ea7ef7b8afc792b027ba4d0cb86f9881bf3cdd
Jlouisa <jonathan.louisa@gmail.com>
2023-08-04 16:47:37 +0200

    Add form and eventlistener

--- javascript.js
+++ javascript.js
@@ -12,6 +12,26 @@ const testLibrary = ["Harry Potter", "Vampire Diaries", "Lord of the Rings"];
 const printBooks = document.querySelector(".books");
 let i = 0;
 
+// ---------------------------
+function authorName(evt) {
+  console.log(evt.key);
+  const key = evt.key;
+}
+
+const authorEl = document.querySelector("#addAuthor");
+authorEl.addEventListener("keydown", authorName);
+// ---------------------------
+function titleName(evt) {
+  console.log(evt.key);
+  const key = evt.key;
+}
+
+const titleEl = document.querySelector("#addTitle");
+titleEl.addEventListener("keydown", titleName);
+// ---------------------------
+
+const addBtn = document.querySelector("addBtn");
+
 testLibrary.forEach((n) => {
   console.log(n);
   let titles = document.createElement("div");
